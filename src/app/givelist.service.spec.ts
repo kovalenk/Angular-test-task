705f0c89cde0366442819444fb427c45c0cfc122
Kovalenko Yuriy <yura.kovalenko.1996.29@gmail.com>
2019-01-08 14:13:49 +0200

    init data component & service

---
+++ src/app/givelist.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { GivelistService } from './givelist.service';
+
+describe('GivelistService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: GivelistService = TestBed.get(GivelistService);
+    expect(service).toBeTruthy();
+  });
+});
